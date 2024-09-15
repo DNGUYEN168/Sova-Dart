f6cb8f6e8dcc643a292f722f515ea8ed69c131d8
DNGUYEN168 <107276510+DNGUYEN168@users.noreply.github.com>
2024-09-14 20:36:47 -0700

    testing files

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(false);
+        
+    }
+
+}
