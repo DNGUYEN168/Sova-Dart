05b5756973b2739bae453df3c4b736b8aa1d51ea
DNGUYEN168 <107276510+DNGUYEN168@users.noreply.github.com>
2024-09-15 19:29:05 -0700

    Foundation
    starting foundation

---
+++ Assets/Scripts/CameraPOS.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPOS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //TODO: add way to attach camera to capsule object 
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update the camera pos to the capsule 
+    }
+}
