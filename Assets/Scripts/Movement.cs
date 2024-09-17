39920e121b0b6b7e93be1f0d2425d77a7dcec4eb
DNGUYEN168 <107276510+DNGUYEN168@users.noreply.github.com>
2024-09-16 20:24:33 -0700

    playermovement finished

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,21 +8,62 @@ public class Movement : MonoBehaviour
 
     [Header("Movement")]
     public float moveSpeed;
+    public float groundDrag;
+
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatisGround;
+    bool isGrounded;
 
     public Transform orientation;
 
     float horizontalInput;
     float verticalInput;
 
+    Vector3 moveDirection;
+
+    Rigidbody rb;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         
+        rb.freezeRotation = true;
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        //take player input and update the position of the capsule per input 
+        MovePlayer();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatisGround);
+
+
+        MyInput();
+
+        //handle drag
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else { rb.drag = 0; }
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection * moveSpeed * 10f, ForceMode.Force);
     }
 }
